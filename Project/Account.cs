0848d1818a5bfaa4626dce0aaeb3ed38c1f41c4b
Vladimir Podolyan <vpodolyan@outlook.com>
2014-06-25 23:02:49 +0400

    init

---
+++ Project/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project
+{
+    public class Account
+    {
+        public int Id { get; private set; }
+        public string Login { get; private set; }
+        public string Password { get; private set; }
+        
+        public void Import()
+        {}
+
+        public bool Export(string login, string pass)
+        {
+            return true;
+        }
+    }
+}
