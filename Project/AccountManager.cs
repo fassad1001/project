338f8427dab31c9c09cc82d08f8cff365c342f2b
fassad1001 <fassad1001@yandex.ru>
2014-06-25 23:31:05 +0400

    ver1beta

---
+++ Project/AccountManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    class AccountManager
+    {
+        public void NewAcc(string login, string password)
+        {
+
+        }
+        public void RemAcc(string login)
+        {
+
+        }
+        public void UpdAcc(string oldLogin, string oldPassword, string newLogin, string newPassword)
+        {
+
+        }
+    }
+}
