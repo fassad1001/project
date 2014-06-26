108d4764d7b76eb022a57cac15cfe7db0f01b013
fassad1001 <fassad1001@yandex.ru>
2014-06-26 18:02:50 +0400

    новые классы и новые переменные

--- Project/InfoBlock.cs
+++ Project/InfoBlock.cs
@@ -8,5 +8,8 @@ namespace Project
 {
     class InfoBlock
     {
+        public string Id { get; private set; }
+        public string Text { get; private set; }
+        public string Pic { get; private set; }
     }
 }
--- Project/Rule.cs
+++ Project/Rule.cs
@@ -8,5 +8,9 @@ namespace Project
 {
     class Rule
     {
+        public int Id { get; private set; }
+        public string Begin { get; private set; }
+        public string End { get; private set; }
+        public string Target { get; private set; }
     }
 }
